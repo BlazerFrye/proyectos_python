5d6ff051c4ee8c7a0850f24fce726119bc28faff
Daniel Aguilar <danni.aguilar.91@gmail.com>
2019-08-31 17:03:21 -0500

    Valores de debug
    Se cambiaron de info a critical los valores de debug

--- Factorial.py
+++ Factorial.py
@@ -7,8 +7,8 @@ def factorial(n):
     total=1
     for i in range(1,n+1):
         total *= i
-        logging.info('i is (%s), total is (%s)'%(i,total))
-    logging.info('Return value is (%s)'%(total))
+        logging.critical('i is (%s), total is (%s)'%(i,total))
+    logging.critical('Return value is (%s)'%(total))
     return total
 print(factorial(5))
 logging.debug('End of program')
